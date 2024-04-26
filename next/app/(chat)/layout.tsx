ed97728906bc6719c9b8f6ff4bcda7a5c916ad6a
laguagu <49702497+laguagu@users.noreply.github.com>
2024-04-26 19:37:20 +0300

    style: Change layout bg to dots

--- next/app/(chat)/layout.tsx
+++ next/app/(chat)/layout.tsx
@@ -1,4 +1,3 @@
-import SparklesUnder from "@/components/chat/sparkles-under";
 import { HomeButton } from "@/components/ui/HomeButton";
 import Link from "next/link";
 import React from "react";
@@ -9,8 +8,8 @@ export default function ChatLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-black w-full flex flex-col items-center justify-center overflow-y-hidden overflow-x-hidden rounded-md">
-      <h1 className="md:text-5xl text-3xl lg:text-7xl font-bold text-centerrelative z-20 mb-3 text-white mt-4">
+    <div className="min-h-screen bg-white bg-dot-black/[0.2] w-full flex flex-col items-center justify-center overflow-y-hidden overflow-x-hidden rounded-md pb-10">
+      <h1 className="md:text-5xl text-3xl lg:text-7xl font-bold text-centerrelative z-20 mb-3 text-black mt-4">
         <Link href={"/"}>Kaluste Assari</Link>
       </h1>
       <div className="grid w-full max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-3xl xl:max-w-6xl grid-cols-1 gap-4 p-4 rounded-lg border-2 shadow-xl border-gray-200 dark:border-gray-800 mx-auto bg-zinc-100">
@@ -23,7 +22,7 @@ export default function ChatLayout({
         </div>
         {children}
       </div>
-      <SparklesUnder />
+
     </div>
   );
 }
